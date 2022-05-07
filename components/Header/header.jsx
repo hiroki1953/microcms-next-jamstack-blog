7346f0e3fe1dbf528def2e2a895717fe4b770095
hiroki1953 <hirokisoccer0709@gmail.com>
2022-05-07 14:53:51 +0900

    ✨ H1にhome画面に戻るリンクの追加

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header className="h-20 flex justify-between items-center border-2 border-gray-300">
-      <h1 className="text-2xl m-8">Tech-DA</h1>
+      <Link href="/">
+        <h1 className="text-2xl m-8">Tech-DA</h1>
+      </Link>
       <Link href="https://twitter.com/vanmason2026">
         <a target="_blank">
           <img src="images/twitter.png" className="h-10 w-12 mr-8"></img>
