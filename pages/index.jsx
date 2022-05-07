2a09a681e3a68f36d53adcda732ab07157d1c087
hiroki1953 <hirokisoccer0709@gmail.com>
2022-05-07 13:25:46 +0900

    ♻️ 拡張子をjsxに変更

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,20 +5,19 @@ import { client } from "libs/client";
 export default function Home({ blog }) {
   return (
     <div className="h-screen w-screen">
-      <div className="mx-0 my-auto text-center">
-        <header className="h-20">
-          <h1>Tech-DA</h1>
-        </header>
-        <ul>
-          {blog.map((blog) => (
-            <li key={blog.id}>
-              <Link href={`/blog/${blog.id}`}>
-                <a>{blog.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <header className="h-20 flex justify-between items-center">
+        <h1 className="text-2xl m-8">Tech-DA</h1>
+        <img src="images/twitter.png" className="m-8 h-10 w-12"></img>
+      </header>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
