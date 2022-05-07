59e667740856f5eef1ff427e8f9a0731e73cd579
hiroki1953 <hirokisoccer0709@gmail.com>
2022-05-07 13:15:30 +0900

    🎨 相対パスから絶対パスに移行

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,4 +1,4 @@
-import { client } from "../../libs/client";
+import { client } from "libs/client";
 
 export default function BlogId({ blog }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
 // pages/index.js
 import Link from "next/link";
-import { client } from "../libs/client";
+import { client } from "libs/client";
 
 export default function Home({ blog }) {
   return (
-    <div>
-      <header>
-        <h1>Tech-DA</h1>
-      </header>
-      <ul>
-        {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>
-              <a>{blog.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="h-screen w-screen">
+      <div className="mx-0 my-auto text-center">
+        <header className="h-20">
+          <h1>Tech-DA</h1>
+        </header>
+        <ul>
+          {blog.map((blog) => (
+            <li key={blog.id}>
+              <Link href={`/blog/${blog.id}`}>
+                <a>{blog.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
