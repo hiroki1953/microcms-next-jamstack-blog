eff46e4727ca5b19fb7db4c18c95ea4783467717
hiroki1953 <hirokisoccer0709@gmail.com>
2022-05-07 16:37:37 +0900

    🐛 ローカルでbuildするためにエラー解消

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,33 @@
 // pages/index.js
-import Link from "next/link";
 import { client } from "libs/client";
-import { Header } from "../components/Header/header";
+import { Header } from "components/Header/header";
+import Link from "next/link";
 
 export default function Home({ blog }) {
   console.log(blog);
   return (
     <div className="h-screen w-screen">
       <Header />
-      <ul className="flex justify-center">
-        {blog.map((blog) => (
-          <li key={blog.id} className="m-5">
-            <Link href={`/blog/${blog.id}`}>
-              <a>
-                <img src={blog.image.url} alt="ダミー" className="w-60 h-50" />
-                {blog.title}
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+
+      <div className="w-2/4 mx-auto my-5">
+        <h2 className="text-2xl text-center">記事一覧</h2>
+        <ul className="grid grid-cols-3">
+          {blog.map((blog) => (
+            <li key={blog.id} className="m-5">
+              <Link href={`/blog/${blog.id}`}>
+                <a>
+                  <img
+                    src={blog.image.url}
+                    alt="ダミー"
+                    className="w-60 h-50"
+                  />
+                  {blog.title}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
