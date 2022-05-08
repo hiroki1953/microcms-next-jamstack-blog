06c09ed9bf8fc4d6083ca7aa4156c11af5828eb3
hiroki1953 <hirokisoccer0709@gmail.com>
2022-05-08 21:55:34 +0900

    💄 headerのレイアウトを変更

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -2,14 +2,9 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="h-20 flex justify-between items-center border-2 border-gray-300">
+    <header className="h-20 flex justify-center items-center border-b-2 border-gray-300">
       <Link href="/">
-        <h1 className="text-2xl m-8">Tech-DA</h1>
-      </Link>
-      <Link href="https://twitter.com/vanmason2026">
-        <a target="_blank">
-          <img src="images/twitter.png" className="h-10 w-12 mr-8"></img>
-        </a>
+        <h1 className="text-3xl m-8">Tech-DA</h1>
       </Link>
     </header>
   );
